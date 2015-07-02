0fd7529cb3fb241b0b5b6c87dbbb4cf9861afa3d
Trisha Andrews <andrews.trisha.a@gmail.com>
2015-07-02 13:35:56 -0400

    added file download and about to delete stuff for ch8

--- turnstiles.py
+++ turnstiles.py
@@ -1,6 +1,8 @@
 import csv
 import datetime
 import random
+import urllib2
+import pycurl
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
@@ -10,9 +12,11 @@ class TurnStiles:
         #self.stations = []
         self.starts = {}
         self.ends = {}
+        self.starts_ends = {}
 
     #Challenge 1
     def read_csv(self, filename):
+        '''takes name of csv file, including extension'''
         #C/A,UNIT,SCP,STATION,LINENAME,DIVISION,DATE,TIME,DESC,ENTRIES,EXITS
         data = {}
         with open(filename, 'rb') as csvfile:
@@ -24,17 +28,16 @@ class TurnStiles:
                 next(incsv)
                 for row in filereader:
                     row = [' '.join(x.split()) for x in row]
-                    #self.stations.append(row[3]) #keep list of stations
                     key = tuple(row[:4])
                     if key not in data:
                         data[key] = [row[4:]]
                     else:
                         data[key].append(row[4:])
-        
         return data
 
     #Challenge 2 
-    def datetime_entries(self, data):
+    def datetime_entries(self, data, entry_in=5):
+        '''takes dictionary, data, converts to datetime, and get count deltas'''
         #csv format: [,,06/20/2015, 00:00:00,,entries,]
         date_time = {}
         outlier = False
@@ -42,21 +45,22 @@ class TurnStiles:
             temp = data[key]
             temp.sort()
             for x in range(len(temp)):
-                #print temp[x]
                 d_t_string = " ".join(temp[x][2:4])
-                d_t = datetime.datetime.strptime(d_t_string, "%m/%d/%Y %H:%M:%S")
+                d_t = datetime.datetime.strptime(d_t_string,"%m/%d/%Y %H:%M:%S")
                 if x > 0:
-                    count = int(temp[x][5]) - int(temp[x-1][5])
+                    count = int(temp[x][entry_in]) - int(temp[x-1][entry_in])
                     val = [d_t, count ]
                     if count < 0 or count > 5000:
                         outlier = True
-                        #pass
                 else:
                     val = [d_t, 0] #could cause problems later
-                    self.starts[key] = temp[x]
+    
                 if not outlier:
-                    if x == len(temp) -1:
-                        self.ends[key] = temp[x]
+                    if x == (len(temp) -1) or x == 0:
+                        if key not in self.starts_ends:
+                            self.starts_ends[key] = [temp[x]]
+                        else:
+                            self.starts_ends[key].append(temp[x])
                     if key not in date_time:
                         date_time[key] = [val]
                     else:
@@ -66,6 +70,7 @@ class TurnStiles:
             
     #Challenge 3, 5, 6
     def day_entries(self, data):
+        '''takes dictionary, data, and combines counts in same date'''
         day_entries = {}
         for key in data:
             vals = data[key]
@@ -74,13 +79,15 @@ class TurnStiles:
             entries = 0
             #print key
             for v in range(len(vals)):
-                nextdate = vals[v][0].date()                
-                #entries += vals[v][1]
+                isdatetime = vals[v][0]
+                if hasattr(isdatetime, 'hour'):
+                    nextdate = vals[v][0].date()
+                else:
+                    nextdate = isdatetime
                 if date is not None:
                     current = vals[v-1]
                     entries += current[1]
                     if date != nextdate:
-                        #print current, date
                         val = [current[0], entries]
                         entries = 0
                         if key not in day_entries:
@@ -88,9 +95,8 @@ class TurnStiles:
                         else:
                             day_entries[key].append(val)
                     if v == len(vals) - 1:
-                        entries += vals[v][1] ##added recently
+                        entries += vals[v][1]
                         val = [vals[v][0], entries]
-                        #print entries
                         entries = 0
                         if key not in day_entries:
                             day_entries[key] = [val]
@@ -99,12 +105,23 @@ class TurnStiles:
                 date = nextdate
         return day_entries
 
+    #Challenge 3.5
+    def day_only(self, data):
+        '''takes dictionary, data, and turnes datetimes into dates'''
+        for key in data:
+            vals = data[key]
+            for v in range(len(vals)):
+                vals[v][0] = vals[v][0].date()
+            data[key] = vals
+        return data
+
     #Challenge 4, 7
     def plot_random(self, data, multiple=False):
         key = random.choice(data.keys())
         terminal = data[key]
         if multiple:
-            self.plot_multiple_weeks(terminal, key)
+            #self.plot_multiple_weeks(terminal, key)
+            pass
         else:
             self.make_graph(terminal, key)
 
@@ -114,7 +131,8 @@ class TurnStiles:
         if key in data:
             terminal = data[key]
             if multiple:
-               self.plot_multiple_weeks(terminal, key)
+                #self.plot_multiple_weeks(terminal, key)
+                pass
             else:
                 self.make_graph(terminal, key)
         else:
@@ -133,33 +151,8 @@ class TurnStiles:
         plt.title(key)
         plt.show()
 
-    #Challenge 8
-    def plot_multiple_weeks(self, terminal, key):
-        #dates = ["Sat", "Sun", "Mon", "Tues", "Wed", "Thur", "Fri"]
-        dates = [0, 1, 2, 3, 4, 5, 6]
-        weeks = []
-        start = terminal[0][0].day % 7 #day of week, usually Saturday
-        temp = []
-        for x in range(len(terminal)):
-            if terminal[x][0].day % 7 == start:
-                if temp:
-                    weeks.append(temp)
-                    temp = []
-            temp.append(terminal[x][1])
-        weeks.append(temp)
-        plt.figure(figsize=(10,3))
-        print weeks
-        for w in weeks:
-            if len(w) == 7:
-                plt.plot(dates,w)
-        plt.title(key)
-        plt.show()
-                
-                
-            
-
     #Challenge 5, 6
-    def combine_terminals(self, data, key_bool, clear=True):
+    def combine_terminals(self, data, key_bool):
         all_terminals = {}
         for key, value in data.items():
             if key_bool == True:
@@ -172,55 +165,107 @@ class TurnStiles:
                 all_terminals[new_key] = all_terminals[new_key] + value
             else:
                 all_terminals[new_key] = value
-        #print all_terminals
         all_terminals = self.day_entries(all_terminals)
-        if clear:
-            data = None
         return all_terminals
 
-    # Challenge 8?
-    def combine_weeks(self, files):
-        d = {}
-        dictlist = []
-        for f in files:
-            di = t.read_csv(f)
-            dictlist.append(di)
-        for dl in dictlist:
-            for key in dl:
-                #print key
-                if key in d:
-                    d[key] += dl[key]
+    # Challenge 8
+    def combine_weeks(self, dicts_list, entry_in=5):
+        #for d in dicts_list:
+        #print self.starts_ends
+        startsends = {}
+        #startsends = self.datetime_entries(self.starts_ends)
+        
+        for key in self.starts_ends:
+            temp = self.starts_ends[key]
+            temp.sort()
+            for x in range(len(temp)):
+                d_t_string = " ".join(temp[x][2:4])
+                d_t = datetime.datetime.strptime(d_t_string,"%m/%d/%Y %H:%M:%S")
+                count = int(temp[x][entry_in])
+                val = [d_t, count]
+                if key not in startsends:
+                    startsends[key] = [val]
                 else:
-                    d[key] = dl[key]
-        return d
+                    startsends[key].append(val)
+        
 
-    def run_all(self, multiple=False):
-        if multiple:
-            files = ["turnstile_150606.txt", "turnstile_150613.txt", "turnstile_150620.txt", "turnstile_150627.txt"]
-            d = self.combine_weeks(files)
-        else:
-            d = t.read_csv("testdata2.txt")
-            #d = t.read_csv("turnstile_150627.txt")        
-        d_t = self.datetime_entries(d)
-        d_e = self.day_entries(d_t)
-        self.plot_random(d_e, multiple=multiple)
+        print "1:\n", startsends
+        #startends = self.day_entries(startends)
+        #startends = self.day_only(startends)
+        #print startends
+        return startsends
+
+    def download_data(self, m, day1, maxday):
+        url = "http://web.mta.info/developers/"
+        path = "data/nyct/turnstile/"
+        month = m
+        day = day1
+        response = urllib2.urlopen(url).read()
+        while day <= maxday:
+            filename = month+ "%02d.txt" %day
+            with open(filename, "wb") as f:
+                curl = pycurl.Curl()
+                curl.setopt(pycurl.URL, url+path+filename)
+                curl.setopt(pycurl.WRITEDATA, f)
+                curl.perform()
+                curl.close()
+            day += 7
+        
+    def challenge_1(self, multiple=False, filename = "turnstile_150627.txt"):
+        #if multiple:
+        #    files = ["turnstile_150606.txt", "turnstile_150613.txt", "turnstile_150620.txt", "turnstile_150627.txt"]
+        #    #data = self.combine_weeks(files)
+        #else:
+        #    #data = t.read_csv("testdata2.txt")
+        data = t.read_csv(filename)
+        return data
+
+    def challenge_3(self, data):
+        d_e = self.day_entries(data)
+        d_o = self.day_only(d_e)
+        return d_o
+
+    def challenge_4(self, data, multiple):
+        self.plot_random(data, multiple=multiple)
         keys = [("A002","R051","02-03-03","LEXINGTON AVE"), ("A002","R051","02-03-01","LEXINGTON AVE")]
         for key in keys:
-            self.plot_specific(d_e, key, multiple=multiple)
-        #print "\n"
-        ac = self.combine_terminals(d_e, True)
-        #print ac
-        #print "#6:"
-        ter = self.combine_terminals(ac, False)
-        self.plot_random(ter, multiple=multiple)
+            self.plot_specific(data, key, multiple=multiple)
+
+    def challenge_7(self, data, multiple):
+        self.plot_random(data, multiple=multiple)
         stations = ["LEXINGTON AVE", "5 AVE-59 ST", "57 ST-7 AVE", "34 ST-HERALD SQ", "28 ST-BROADWAY"]
         for place in stations:
-            self.plot_specific(ter, place, multiple=multiple)
+            self.plot_specific(data, place, multiple=multiple)
 
+    def challenge_8(self):
+        files = ["turnstile_150606.txt", "turnstile_150613.txt", "turnstile_150620.txt", "turnstile_150627.txt"]
+        files.sort()
+        dicts = []
+        for f in files:
+            d = self.read_csv(f)
+            dicts.append(d)
+        for d in dicts:
+            d = self.datetime_entries(d)
+        self.combine_weeks(dicts)
+        #return dicts
+
+    def run_all(self, multiple=False):
+        #self.download_data("turnstile_1504", 4, 30)
+        #self.download_data("turnstile_1505", 2, 31)
+        #d = self.challenge_1(multiple=multiple)
+        #d_t = self.datetime_entries(d)
+        #c3 = self.challenge_3(d_t)
+        #self.challenge_4(c3, multiple=multiple)
+        #c5 = self.combine_terminals(c3, True)
+        #c6 = self.combine_terminals(c5, False)
+        #self.challenge_7(c6, multiple=multiple)
+        self.challenge_8()
+
+        
 
 if __name__ == "__main__":
     t = TurnStiles()
-    t.run_all(multiple=True)
+    t.run_all(multiple=False)
 
     ##TODO linter emacs
     ##TODO eliminate 0 <= count <= 5000 in challenge 2
